146acb10a4a20a366385d67136db88fdbdc34eda
Sagar Gupta <sg85207@gmail.com>
2025-11-29 09:45:37 +0530

    Phase 2: Move app entry points

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "../styles/index.css";
 import App from "./App";
-import EnhancedErrorBoundary from "./shared/components/EnhancedErrorBoundary";
+import EnhancedErrorBoundary from "../shared/components/EnhancedErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
